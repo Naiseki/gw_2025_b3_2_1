0be8175f65b0970cd6ea7f932c6ba07b73bda5a8
kitahara <kitahara@ai.cs.ehime-u.ac.jp>
2025-12-11 00:31:37 +0900

    ジャーナル名辞書を読み込む関数を追加

---
+++ load_resource.py
@@ -0,0 +1,14 @@
+import json
+
+
+_journal_name_dict: dict[str, str] = None
+
+def load_journal_name_dict() -> dict[str, str] | None:
+    if _journal_name_dict is None:
+        try:
+            with open("resources/journal_names.json", "r") as f:
+                _journal_name_dict = json.load(f)
+        except FileNotFoundError:
+            print("警告: resources/journal_names.json が見つかりません。")
+
+    return _journal_name_dict
